5dd90b5affa6b27de5badbb3a958435d0133a007
Yj <yejiestudy@outlook.com>
2025-12-31 04:12:16 +0000

    refactor: Replace Glue Crawler with Partition Projection Table

--- infra/envs/dev/glue.tf
+++ infra/envs/dev/glue.tf
@@ -41,14 +41,54 @@ resource "aws_glue_catalog_database" "crypto_db" {
   name = "crypto_lake_db"
 }
 
-# --- 5. 创建爬虫 (Crawler) ---
-resource "aws_glue_crawler" "bronze_crawler" {
-  name          = "crypto-bronze-crawler"
-  role          = aws_iam_role.glue_crawler_role.arn
+
+#  --- 5. Serverless 分区投影表 (0成本、0延迟) ---
+resource "aws_glue_catalog_table" "raw_table" {
+  name          = "raw"
   database_name = aws_glue_catalog_database.crypto_db.name
 
-  # 告诉爬虫去哪里扫描数据
-  s3_target {
-    path = "s3://${aws_s3_bucket.bronze.bucket}/raw/"
+  table_type = "EXTERNAL_TABLE"
+
+  parameters = {
+    "classification"                  = "json"
+    "projection.enabled"              = "true"
+    "projection.date.type"            = "date"
+    "projection.date.format"          = "yyyy-MM-dd"
+    "projection.date.range"           = "2024-01-01,NOW"
+    "projection.date.interval"        = "1"
+    "projection.date.interval.unit"   = "DAYS"
+    "storage.location.template"       = "s3://${aws_s3_bucket.bronze.bucket}/raw/$${date}/"
+  }
+
+  storage_descriptor {
+    location      = "s3://${aws_s3_bucket.bronze.bucket}/raw/"
+    input_format  = "org.apache.hadoop.mapred.TextInputFormat"
+    output_format = "org.apache.hadoop.hive.ql.io.HiveIgnoreKeyTextOutputFormat"
+
+    ser_de_info {
+      name                  = "json-serde"
+      serialization_library = "org.openx.data.jsonserde.JsonSerDe"
+    }
+
+    columns {
+      name = "bitcoin"
+      type = "struct<usd:double,last_updated_at:bigint>"
+    }
+
+    columns {
+      name = "ethereum"
+      type = "struct<usd:double,last_updated_at:bigint>"
+    }
+
+    # 这里的 date 是分区键
+    columns {
+        name = "date"
+        type = "string"
+    }
+  }
+
+  partition_keys {
+    name = "date"
+    type = "string"
   }
 }
